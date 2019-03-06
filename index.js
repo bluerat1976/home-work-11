9f40f52d8d4843ee132ecbc5658eb5fe73f15bbe
bluerat1976 <osoblivaia76@mail.ru>
2019-03-06 14:01:55 +0200

    Finished home work with switch operator

---
+++ index.js
@@ -0,0 +1,31 @@
+//  with "if ... else" operator.
+var day = prompt('Enter day of week', '');
+
+if(day == 'Monday' || day == 'Tuesday' || day == 'Wednesday' || day == 'Thursday' || day == 'Friday') {
+    alert('Working day');
+} else if(day == 'Saturday' || day == 'Sunday') {
+    alert('Weekend');
+} else {
+    alert('Can not define. Please try another value.')
+}
+
+// with "switch" operator.
+
+switch(day) {
+    case 'Monday':
+    case 'Tuesday':
+    case 'Wednesday':
+    case 'Thursday':
+    case 'Friday':
+        alert('Working day');
+        break;
+
+    case 'Saturday':
+    case 'Sunday':
+        alert('Weekend');
+        break;
+
+    default:
+        alert('Can not define. Please try another value.');
+        break;
+}
